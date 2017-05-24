3b299ebe7ff1561ed744a6ad009cd3d2df1a42c3
lionel <laura.ionel@1and1.ro>
2017-05-24 19:39:27 +0300

    2

--- msg-tuto/src/masterworker0.c
+++ msg-tuto/src/masterworker0.c
@@ -14,6 +14,37 @@
 XBT_LOG_NEW_DEFAULT_CATEGORY(msg_test,
                              "Messages specific for this msg example");
 
+int communicator_size = 0;
+typedef struct {
+  int last_Irecv_sender_id;
+  int bcast_counter;
+  int reduce_counter;
+  int allReduce_counter;
+  xbt_dynar_t isends;           /* of msg_comm_t */
+  /* Used to implement irecv+wait */
+  xbt_dynar_t irecvs;           /* of msg_comm_t */
+  xbt_dynar_t tasks;            /* of msg_task_t */
+} s_process_globals_t, *process_globals_t;
+
+// #define ACT_DEBUG(...) \
+//   if (XBT_LOG_ISENABLED(actions, xbt_log_priority_verbose)) {  \
+//     char *NAME = xbt_str_join_array(action, " ");              \
+//     XBT_DEBUG(__VA_ARGS__);                                    \
+//     xbt_free(NAME);                                            \
+//   } else ((void)0)
+
+
+/* Helper function */
+static double parse_double(const char *string)
+{
+  double value;
+  char *endptr;
+
+  value = strtod(string, &endptr);
+  if (*endptr != '\0')
+    THROWF(unknown_error, 0, "%s is not a double", string);
+  return value;
+}
 
 typedef struct task_data {
 
@@ -24,7 +55,8 @@ typedef struct task_data {
 int master(int argc, char *argv[]);
 int worker(int argc, char *argv[]);
 msg_error_t test_all(const char *platform_file,
-                     const char *application_file);
+                     const char *application_file,
+                     char *action_file);
 
 #define FINALIZE ((void*)221297)        /* a magic number to tell people to stop working */
 
@@ -116,6 +148,65 @@ int master(int argc, char *argv[])
   return 0;
 }                               /* end_of_master */
 
+static void log_action(const char *const *action, double date)
+{
+  // if (XBT_LOG_ISENABLED(actions, xbt_log_priority_verbose)) {
+    char *name = xbt_str_join_array(action, " ");
+    XBT_INFO("%s %f", name, date);
+    xbt_free(name);
+  // }
+}
+
+void broadcast_message(const char *const *action) {
+    int i;
+  char *bcast_identifier;
+  char mailbox[80];
+  double comm_size = parse_double(action[2]);
+  msg_task_t task = NULL;
+  const char *process_name;
+  double clock = MSG_get_clock();
+
+  process_globals_t counters =
+      (process_globals_t) MSG_process_get_data(MSG_process_self());
+
+  xbt_assert(communicator_size, "Size of Communicator is not defined, "
+             "can't use collective operations");
+
+  process_name = MSG_process_get_name(MSG_process_self());
+
+  bcast_identifier = bprintf("bcast_%d", counters->bcast_counter++);
+
+  if (!strcmp(process_name, "p0")) {
+    XBT_DEBUG("%s: %s is the Root", bcast_identifier, process_name);
+
+    msg_comm_t *comms = xbt_new0(msg_comm_t, communicator_size - 1);
+
+    for (i = 1; i < communicator_size; i++) {
+      sprintf(mailbox, "%s_p0_p%d", bcast_identifier, i);
+      comms[i - 1] =
+          MSG_task_isend(MSG_task_create(mailbox, 0, comm_size, NULL), mailbox);
+    }
+    MSG_comm_waitall(comms, communicator_size - 1, -1);
+    for (i = 1; i < communicator_size; i++)
+      MSG_comm_destroy(comms[i - 1]);
+    xbt_free(comms);
+
+    XBT_DEBUG("%s: all messages sent by %s have been received",
+              bcast_identifier, process_name);
+
+  } else {
+    sprintf(mailbox, "%s_p0_%s", bcast_identifier, process_name);
+    MSG_task_receive(&task, mailbox);
+    MSG_task_destroy(task);
+    XBT_DEBUG("%s: %s has received", bcast_identifier, process_name);
+  }
+
+  log_action(action, MSG_get_clock() - clock);
+  xbt_free(bcast_identifier);
+}
+
+
+
 /** Receiver function  */
 int worker(int argc, char *argv[])
 {
@@ -148,6 +239,33 @@ int worker(int argc, char *argv[])
   host_list = MSG_hosts_as_dynar();
   const char *myName = MSG_host_get_name(MSG_host_self());
 
+
+if (!strcmp(process_name, "p0")) {
+    XBT_DEBUG("%s: %s is the Root", bcast_identifier, process_name);
+
+    msg_comm_t *comms = xbt_new0(msg_comm_t, communicator_size - 1);
+
+    for (i = 1; i < communicator_size; i++) {
+      sprintf(mailbox, "%s_p0_p%d", bcast_identifier, i);
+      comms[i - 1] =
+          MSG_task_isend(MSG_task_create(mailbox, 0, comm_size, NULL), mailbox);
+    }
+    MSG_comm_waitall(comms, communicator_size - 1, -1);
+    for (i = 1; i < communicator_size; i++)
+      MSG_comm_destroy(comms[i - 1]);
+    xbt_free(comms);
+
+    XBT_DEBUG("%s: all messages sent by %s have been received",
+              bcast_identifier, process_name);
+
+  } else {
+    sprintf(mailbox, "%s_p0_%s", bcast_identifier, process_name);
+    MSG_task_receive(&task, mailbox);
+    MSG_task_destroy(task);
+    XBT_DEBUG("%s: %s has received", bcast_identifier, process_name);
+  }
+
+
   xbt_dynar_foreach(host_list, i, host) {
     const char *descr = MSG_host_get_name(host);
 //    MSG_host_set_property_value(host, "stream", (char*)stream, NULL);
@@ -156,6 +274,8 @@ int worker(int argc, char *argv[])
       XBT_INFO("Sending \"%s\" to \"%s\"", task->name, descr);
       MSG_task_send(task, descr);
       XBT_INFO("Sent"); 
+    } else {
+      XBT_INFO("+++NOT SENT TO %s", descr);
     }
 
   }
@@ -163,6 +283,7 @@ int worker(int argc, char *argv[])
 
     MSG_task_destroy(task);
     task = NULL;
+    break;
   }
   XBT_INFO("I'm done. See you!");
   return 0;
@@ -170,7 +291,8 @@ int worker(int argc, char *argv[])
 
 /** Test function */
 msg_error_t test_all(const char *platform_file,
-                     const char *application_file)
+                     const char *application_file, 
+                     char *action_file)
 {
   msg_error_t res = MSG_OK;
 
@@ -183,6 +305,10 @@ msg_error_t test_all(const char *platform_file,
 
 
     MSG_launch_application(application_file);
+
+    //xbt_replay_action_register("reduce", action_reduce);
+
+    MSG_action_trace_run(action_file);
   }
   res = MSG_main();
 
@@ -202,7 +328,7 @@ int main(int argc, char *argv[])
     printf("example: %s msg_platform.xml msg_deployment.xml\n", argv[0]);
     exit(1);
   }
-  res = test_all(argv[1], argv[2]);
+  res = test_all(argv[1], argv[2], argv[3]);
 
   if (res == MSG_OK)
     return 0;
